ec8b34bba9b495e49d7275f81e1be3430d844e1a
Hilbert Yang <163887234+HilbertYang@users.noreply.github.com>
2025-08-06 16:43:54 +0800

    Create Solution1.v

---
+++ Circuits/Shift registers/4-bit shift register/Solution1.v
@@ -0,0 +1,27 @@
+module top_module(
+    input clk,
+    input areset,  // async active-high reset to zero
+    input load,
+    input ena,
+    input [3:0] data,
+    output reg [3:0] q); 
+    always @(posedge clk)begin
+        if(areset)begin
+        q <= 0;
+        end
+        else begin
+            if(load)begin
+                q <= data;
+            end
+            else if (ena)begin
+                q[3]<=1'b0;
+                q[2:0]<=q[3:1];
+            end
+            else begin
+                q<=q;
+            end
+        end
+    end
+    
+
+endmodule
